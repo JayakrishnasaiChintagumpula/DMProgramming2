14c3228c233b7baa966375f6fa3b0b98fd4412ef
JayakrishnasaiChintagumpula <146905031+JayakrishnasaiChintagumpula@users.noreply.github.com>
2024-03-31 19:51:52 -0400

    Update part2.py

--- part2.py
+++ part2.py
@@ -36,20 +36,23 @@ In this task you will explore different methods to find a good value for k
 # Change the arguments and return according to 
 # the question asked. 
 
+"""
+Part 2
+Comparison of Clustering Evaluation Metrics: 
+In this task you will explore different methods to find a good value for k
+"""
+
+# Fill this function with code at this location. Do NOT move it. 
+# Change the arguments and return according to 
+# the question asked. 
+
 def fit_kmeans(data, n_clusters):
-    scaler = StandardScaler()
-    standardized_data = scaler.fit_transform(data)
-    
-    kmeans = KMeans(n_clusters=n_clusters, init='random', random_state=0)
-    kmeans.fit(standardized_data)
-
-    predicted_labels = kmeans.labels_
-
+    kmeans = KMeans(n_clusters=n_clusters, random_state=12)
+    kmeans.fit(data)
     centroids = kmeans.cluster_centers_
-    distance = np.linalg.norm(standardized_data - centroids[predicted_labels], axis=1)
-    sse = np.sum(distance**2)
-    return sse
-
+    labels = kmeans.labels_
+    SSE = np.sum((data - centroids[labels]) ** 2)
+    return SSE
 
 
 def compute():
@@ -57,66 +60,55 @@ def compute():
     answers = {}
 
     """
-    A.	Call the make_blobs function with following parameters :(center_box=(-20,20), n_samples=20, centers=5, random_state=12).
+    A. Call the make_blobs function with following parameters: (center_box=(-20,20), n_samples=20, centers=5, random_state=12).
     """
+    X, y = make_blobs(center_box=(-20, 20), n_samples=20, centers=5, random_state=12)
+    # Here we assume centers are fixed for a given random_state and parameters
+    kmeans = KMeans(n_clusters=5, random_state=12).fit(X)
+    centers = kmeans.cluster_centers_
+    dct = answers["2A: blob"] = [X, y, centers]
+    ...
 
-    # dct: return value from the make_blobs function in sklearn, expressed as a list of three numpy arrays
-    np.random.seed(12)
-    data, labels = make_blobs(n_samples=20, centers=5, center_box=(-20, 20), random_state=12)
-    dct = answers["2A: blob"] = [data,labels]
 
     """
     B. Modify the fit_kmeans function to return the SSE (see Equations 8.1 and 8.2 in the book).
     """
-
-    # dct value: the `fit_kmeans` function
+    # The function `fit_kmeans` is already modified above to return the SSE.
     dct = answers["2B: fit_kmeans"] = fit_kmeans
 
     """
-    C.	Plot the SSE as a function of k for k=1,2,….,8, and choose the optimal k based on the elbow method.
+    C. Plot the SSE as a function of k for k=1,2,….,8, and choose the optimal k based on the elbow method.
     """
-
-    # dct value: a list of tuples, e.g., [[0, 100.], [1, 200.]]
-    # Each tuple is a (k, SSE) pair
     sse_values = []
     for k in range(1, 9):
-        sse = fit_kmeans(data, k)
-        sse_values.append([k, sse])
+        sse = fit_kmeans(X, k)
+        sse_values.append((k, sse))
     
-    # Plotting the SSE values
-    plt.figure(figsize=(8, 6))
-    plt.plot([k for k, _ in sse_values], [sse for _, sse in sse_values], '-o')
-    plt.title('SSE as function of k')
-    plt.xlabel('Number of clusters k')
+    answers["2C: SSE plot"] = sse_values
+    plt.plot([k[0] for k in sse_values], [k[1] for k in sse_values], 'bx-')
+    plt.xlabel('k')
     plt.ylabel('SSE')
-    plt.grid(True)
+    plt.title('The Elbow Method showing the optimal k')
     plt.show()
+
     dct = answers["2C: SSE plot"] = sse_values
 
     """
-    D.	Repeat part 2.C for inertia (note this is an attribute in the kmeans estimator called _inertia). Do the optimal k’s agree?
+    D. Repeat part 2.C for inertia (note this is an attribute in the kmeans estimator called _inertia). Do the optimal k’s agree?
     """
-
-    # dct value has the same structure as in 2C
     inertia_values = []
     for k in range(1, 9):
-        kmeans = KMeans(n_clusters=k, random_state=0, init="random")
-        kmeans.fit(data)
-        inertia = kmeans.inertia_
-        inertia_values.append([k, inertia])
+        kmeans = KMeans(n_clusters=k, random_state=12)
+        kmeans.fit(X)
+        inertia_values.append((k, kmeans.inertia_))
     
-    # Plotting the inertia values
-    plt.figure(figsize=(8, 6))
-    plt.plot([k for k, _ in inertia_values], [inertia for _, inertia in inertia_values], '-o')
-    plt.title('Inertia as function of k')
-    plt.xlabel('Number of clusters k')
-    plt.ylabel('Inertia')
-    plt.grid(True)
-    plt.show()
-    dct = answers["2D: inertia plot"] = inertia_values
-
-    # dct value should be a string, e.g., "yes" or "no"
-    dct = answers["2D: do ks agree?"] = "yes"
+    answers["2D: inertia plot"] = inertia_values
+    # Comparing SSE and inertia to see if the optimal k values agree.
+    optimal_sse_k = min(sse_values, key=lambda t: t[1])[0]
+    optimal_inertia_k = min(inertia_values, key=lambda t: t[1])[0]
+    dct = answers["2C: SSE plot"] = inertia_values
+    
+    dct = answers["2D: do ks agree?"] = "yes" if optimal_sse_k == optimal_inertia_k else "no"
 
     return answers
 
